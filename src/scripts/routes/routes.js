57bcbeb5a5b8f4918ce64c015f26ced647efa7ff
heronhoga <nozzeravixer@gmail.com>
2025-05-26 13:00:19 +0700

    feat:add register page

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,9 +1,11 @@
 import HomePage from '../pages/home/home-page';
 import LoginPage from '../pages/login/login-page';
+import RegisterPage from '../pages/register/register-page';
 
 const routes = {
   '/': new HomePage(),
   '/login': new LoginPage(),
+  '/register': new RegisterPage()
 };
 
 export default routes;
